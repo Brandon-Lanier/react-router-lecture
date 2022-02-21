ae4cfeb09470070c44d14ce4a65f65b2e9fe0d94
Brandon Lanier <brandonjlanier@gmail.com>
2022-02-21 15:45:08 -0600

    Finalized

---
+++ src/components/AnimalDetail/AnimalDetail.jsx
@@ -0,0 +1,18 @@
+
+import { useParams } from 'react-router-dom';
+
+function AnimalDetail() {
+
+    const {id} = useParams();
+
+    return (
+        <>
+            <h1>Animal Details</h1>
+            <p>Details for animal with id of {id}</p>
+        </>
+
+    )
+
+}
+
+export default AnimalDetail;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 function Home() {
+
+const history = useHistory();
+
+  const handleClick = () => {
+    alert('You clicked me!')
+    // change location
+    history.push('/animals')
+  }
+
+  
   return (
     <div>
       <h1>HOME</h1>
+      <button onClick={handleClick}>Lets Go!</button>
       <img src="https://pixy.org/download/4718707/" width={500}/>
     </div>
   );
